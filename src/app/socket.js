d086d4b18169594bfb215dd3e194ffb0c243d532
jluccisano <joseph.luccisano@gmail.com>
2017-02-12 17:22:00 +0100

    install es6-request

--- src/app/socket.js
+++ src/app/socket.js
@@ -4,7 +4,9 @@
 import React from 'react';
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
-import ReactD3Basic from 'react-d3-basic';
+import Request from 'es6-request';
+import {LineChart} from 'react-d3-basic';
+import d3 from 'd3';
 
 class SocketClient extends React.Component {
   constructor(props, context) {
@@ -12,30 +14,7 @@ class SocketClient extends React.Component {
 
     this.state = {
       dht22: {temperature: 0, humidity: 0},
-      LineChart: ReactD3Basic.LineChart,
-      data: [
-        {
-          age: 39,
-          index: 0
-        },
-        {
-          age: 38,
-          index: 1
-        }
-      ],
-      chartSeries: [{
-        field: 'age',
-        name: 'Age',
-        color: '#ff7f0e',
-        style: {
-          "stroke-width": 2,
-          "stroke-opacity": 0.2,
-          "fill-opacity": 0.2
-        }
-      }]
-      x(d) {
-        return d.index;
-      }
+      dataProvider: []
     };
 
     this.socket = new SockJS('http://nextrun.fr:8084/stomp');
@@ -48,38 +27,51 @@ class SocketClient extends React.Component {
         this.setState({dht22: JSON.parse(data.body).data});
       });
     });
+  }
 
-    fetch("http://127.0.0.1:8084/api/v1/sensor/continuous", {
-      method: 'get',
-      mode: 'no-cors',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-    .then(response => {
-      if (response.status >= 400) {
-        throw new Error("Bad response from server");
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log(data);
-    });
+  componentDidMount() {
+    Request.get("http://localhost:8084/api/v1/sensor/dht22/fresh/raspberry_1")
+      .then(response => {
+        console.log(JSON.parse(response[0]));
+        this.setState({dht22: JSON.parse(response[0])});
+      });
+    Request.get("http://localhost:8084/api/v1/sensor/dht22/continuous/raspberry_1?sample=1h&range=12h")
+      .then(response => {
+        this.setState({dataProvider: JSON.parse(response[0]).values});
+      });
   }
 
   render() {
     const dht22 = this.state.dht22;
-    const chartSeries = this.state.chartSeries;
-    const data = this.state.data;
-    const LineChart = this.state.LineChart;
-    const x = this.state.x;
+    const dataProvider = this.state.dataProvider;
 
+    const chartSeries = [
+      {
+        field: 'mean_temperature',
+        name: 'Temperature',
+        color: '#ff7f0e',
+        style: {
+          strokeWidth: 2,
+          strokeOpacity: 0.2,
+          fillOpacity: 0.2
+        }
+      }
+    ];
+
+    const parseISODate = d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse;
+
+    const x = function (d) {
+      return parseISODate(d.time);
+    };
+
+    const yScale = d3.scaleLinear()
+      .range([300, 0])
+      .domain([0, 50]);
     return (
       <div>
         <h2>Temperature: {dht22.temperature.toFixed(2)} °C</h2>
         <h2>Humidity: {dht22.humidity.toFixed(2)} %</h2>
-        <LineChart width={600} height={300} data={data} chartSeries={chartSeries} x={x}/>
+        <LineChart width={1900} height={300} data={dataProvider} chartSeries={chartSeries} x={x} xScale={"time"} yScale={yScale}/>
       </div>
     );
   }
